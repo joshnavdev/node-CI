f2e9c21daeda786c33e71d9a3bd6e4ef04c05413
Joshua Navarro <joshuanavarro@Joshuas-MacBook-Air.local>
2018-11-02 13:58:48 -0500

    simple node app with test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../index');
+
+describe('GET /', function() {
+    it('respond with Hello World', function(done) {
+        request(app).get('/').expect('Hello world', done);
+    });
+});
